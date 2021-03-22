afa324a442b541717033caf3f185870058546c98
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-03-22 21:00:25 +0800

    home add state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import { Layout } from 'antd';
+import { useState } from 'react';
 import AppHeader from "../components/Header"
+import NavBar from "../components/NavBar"
 import AppFooter from "../components/Footer"
 import ProductList from "../components/ProductList";
 import products from "../json/products.json";
@@ -9,10 +11,16 @@ const { Header, Content, Footer } = Layout;
 
 
 function Home() {
+  const [isOnTouch, setIsOnTouch] = useState(false);
   return (
     <Layout className="container main-layout">
       <Header className="layout-header">
+        <HamMenu
+          onClick={()=> setIsOnTouch(!isOnTouch)}
+          isOnTouch={isOnTouch}
+        />
         <AppHeader title="NORDIC NEST Shopping Cart"/>
+        <NavBar isOnTouch={isOnTouch}/>
       </Header>
       <Content className="layout-content">
         <ProductList products={products}/>
